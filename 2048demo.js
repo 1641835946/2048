c065a1ec94453efef7268bd1a3026c9141b36e93
1641835946 <1641835946@qq.com>
2018-06-05 17:34:27 +0800

    发现了一个错误逻辑，添加菜单栏

--- 2048demo.js
+++ 2048demo.js
@@ -1,5 +1,20 @@
-
 window.onload=function () {
+  initial();
+}
+
+var panelContent;
+var score;
+var awardScore;
+var initial=function(){
+  $("#bg").empty();
+  panelContent=[
+                  [0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0] ];
+  awardScore=0;
+  score=0;
+  $("#scoreText").text("0");
   for(var i=0; i<4; i++) {
     $("#bg").append("<div id=i"+i+'></div>');
     for(var j=0; j<4; j++) {
@@ -13,50 +28,48 @@ window.onload=function () {
 }
 
 document.onkeydown=function(){
-  switch(isFull){
-  //游戏结束提示
-    case 0:
-      alert("Game Over!");
-      return;
-    case 1:
-      alert("Congratulation!");
-      return;
-    case 2:
-      break;
-  }
   switch(event.keyCode){
     case 37: //左键
       if(canLeft()) {
         moveLeft();
+      } else {
+        return;
       }
       break;
     case 38: //向上键
       if(canUp()) {
         moveUp();
+      } else {
+        return;
       }
       break;
     case 39: //右键
       if(canRight()) {
         moveRight();
+      } else {
+        return;
       }
       break;
     case 40: //向下键
       if(canDown()) {
         moveDown();
+      } else {
+        return;
       }
       break;
     default:
       return;
   }
+  // window.setTimeout("awardAnimate()", 200);
+  awardAnimate();
   randomNum();
+  isFull();
   show();
+  // window.setTimeout("isFull()",600);
 }
-var panelContent=[
-                  [0,0,0,0],
-                  [0,0,0,0],
-                  [0,0,0,0],
-                  [0,0,0,0] ];
+
 var randomNum=function(){
+  //随机出现的数
   var blankNum=0;
   for(var i=0; i<4; i++) {
     for(var j=0; j<4; j++) {
@@ -67,13 +80,14 @@ var randomNum=function(){
   }  
   var numZ=Math.round(Math.random()*blankNum);
   if(numZ==0) numZ=1;
-  if(Math.random()<0.7) {//加入2
-    addNum(2,numZ);
+  if(Math.random()<0.5) {//加入2
+    randomPos(2,numZ);
   } else {//加入4
-    addNum(4,numZ);
+    randomPos(4,numZ);
   }
 }
-var addNum=function(num,numZ){
+var randomPos=function(num,numZ){
+  //随机出现的位置
   var z=0;
   for(var i=0; i<4; i++) {
     for(var j=0; j<4; j++) {
@@ -89,18 +103,28 @@ var addNum=function(num,numZ){
 }
 
 var isFull=function(){
-  if(canLeft()||canUp()||canRight()||canDown()) {
-    return 0;
-  } else if(reach2048()) {
-    return 1;
+  //游戏结束提示
+  if(reach2048()){
+    alert("Congratulation!");
+  } else if(canLeft()||canUp()||canRight()||canDown()) {
+    //不如这个效率高!canMoveLeft(checkerboard) && !canMoveUp(checkerboard) && !canMoveRight(checkerboard) && !canMoveDown(checkerboard) 
+    return;
   } else {
-    return 2;
+    alert("Game Over!");
   }
 }
-//*************************************************************************************************************************
+
 var reach2048=function(){
+  for(var i=0; i<4; i++) {
+    for(var j=0; j<4; j++) {
+      if(panelContent[i][j]==2048) {
+        return true;
+      }
+    }
+  }
   return false;
 }
+
 var canLeft=function(){
 //是否可以向左合并或向左移动，成功返回true，失败返回false。
   for(var i=0; i<4; i++) {
@@ -162,16 +186,30 @@ var turnUp=function(i, j){
 var turnDown=function(i, j){
   //向下滑动
 }
+var awardAnimate = function(){
+  //加分动画
+  if(awardScore>0){
+    score+=awardScore;
+    $("#scoreText").text(score); //总分
+    $("#awardScore").text("+"+awardScore);//加分
+    $("#awardScore").addClass("awardAnimate");
+    window.setTimeout(function(){
+        $("#awardScore").text("");
+        $("#awardScore").removeClass("awardAnimate");  
+        awardScore=0;
+    }, 400);
+  }
+}
 var emphasize=function(i, j){//需修改
-  //放大动画
+  //合并动画
+  awardScore+=panelContent[i][j];
   $("#i"+i+"j"+j).addClass("emphasize");
-  window.setTimeout("shrink("+i+","+j+")", 100);
+  window.setTimeout(function(){
+    $("#i"+i+"j"+j).addClass("shrink");
+    window.setTimeout("removeEmphasize("+i+","+j+")", 100);
+  }, 100);
 }
-var shrink=function(i, j){
-  //缩小动画
-  $("#i"+i+"j"+j).addClass("shrink");
-  window.setTimeout("removeEmphasize("+i+","+j+")", 100);
-}
+
 var removeEmphasize=function(i, j){
   //动画结束后移除产生动画的样式
   $("#i"+i+"j"+j).removeClass("emphasize");
@@ -179,12 +217,11 @@ var removeEmphasize=function(i, j){
 }
 var appear=function(i, j){
   //随机出现方块的动画
-  $("#i"+i+"j"+j).addClass("appear");
-  $("#i"+i+"j"+j).removeClass("appear");
+  // $("#i"+i+"j"+j).addClass("appear");
+  // $("#i"+i+"j"+j).removeClass("appear");
 }
 var removeSquBg=function(i, j){
   $("#i"+i+"j"+j).removeClass("num"+panelContent[i][j]);
-  $("#i"+i+"j"+j).addClass("num0");
 }
 var leftAnimate=function(i, j, m){
   // 滑出的动画，去掉底色
@@ -210,6 +247,33 @@ var downAnimate=function(i, m, j){
 var moveLeft=function(){
   //一共三种动画，放大出现、移动消失、合并（移动消失+扩放出现）
   // 从最左侧开始，为0则移动到此位置，相同则合并，直到不能合并为止。然后到下一个方块检测移动，检测是否可以合并。
+  // 应修改为如下方法，判断是否移动或合并以及检查中间是否存在数字。
+  /*for(var i=0;i<4;i++){
+    for(var j=1;j<4;j++){
+     if(checkerboard[i][j] != 0){
+      for(var k=0;k<j;k++){
+       if(checkerboard[i][k] == 0 && noMiddleNumRow(i,k,j,checkerboard)){
+        moveAnimation(i,j,i,k);
+        checkerboard[i][k] = checkerboard[i][j];
+        checkerboard[i][j] = 0;
+       }else if(checkerboard[i][k] == checkerboard[i][j] && noMiddleNumRow(i,k,j,checkerboard) && !hasConflicted[i][k]){
+        moveAnimation(i,j,i,k);
+        checkerboard[i][k] += checkerboard[i][j];
+        checkerboard[i][j] = 0;
+       }
+      }
+     }
+    }
+  }
+  // 判断中间的数字格是否为0(行)
+  function noMiddleNumRow(row,col1,col2,checkerboard){
+   for(var i=col1+1;i<col2;i++){
+    if(checkerboard[row][i] != 0){
+     return false;
+    }
+   }
+   return true;
+  }*/
   for(var i=0; i<4; i++) {
     for(var j=0; j<3; j++) {
       var hasNum=false;
@@ -356,6 +420,7 @@ var moveDown=function(){
 }
 
 var show=function(){
+  // 可以改成返回对应的颜色，这样与动画的顺序会和谐，还不需要在移动和合并的时候去掉原来的类。
   for(var i=0; i<4; i++) {
     for(var j=0; j<4; j++) {
       if(panelContent[i][j]>0) {
